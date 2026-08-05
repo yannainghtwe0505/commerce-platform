3053c1be27daa9d9eb6d0146b7da0f9089cc005c
Yan Naing Htwe <ynainghtwe61@gmail.com>
2026-08-05 22:29:27 +0900

    Initial Spring Boot project

---
+++ src/main/java/commerce/yan/platform/CommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package commerce.yan.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/commerce/yan/platform/CommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package commerce.yan.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
